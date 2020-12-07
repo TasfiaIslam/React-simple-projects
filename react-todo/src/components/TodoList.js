2655e98f936eaa737424489b1328cae70639da7e
Tasfia Islam <tasf1a027@gmail.com>
2020-12-08 04:54:11 +0600

    Add dynamic input text

--- react-todo/src/components/TodoList.js
+++ react-todo/src/components/TodoList.js
@@ -5,7 +5,9 @@ const TodoList = ({ todos }) => {
   return (
     <div className="todo-container">
       <ul className="todo-list">
-        <Todo />
+        {todos.map((todo) => (
+          <Todo key={todo.id} text={todo.text} />
+        ))}
       </ul>
     </div>
   );
