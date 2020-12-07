ed9112fd88b56f5ba6f6e1064f4c512a322d87b5
Tasfia Islam <tasf1a027@gmail.com>
2020-12-08 01:53:36 +0600

    Add todo

--- react-todo/src/components/Form.js
+++ react-todo/src/components/Form.js
@@ -1,13 +1,27 @@
 import React from "react";
 
-const Form = () => {
+const Form = ({ inputText, setInputText, todos, setTodos }) => {
   const inputTextHandler = (e) => {
     console.log(e);
+    setInputText(e.target.value);
+  };
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      { text: inputText, completed: false, id: Math.random() * 10 },
+    ]);
+    setInputText("");
   };
   return (
     <form>
-      <input type="text" className="todo-input" />
-      <button className="todo-button" type="submit">
+      <input
+        value={inputText}
+        onChange={inputTextHandler}
+        type="text"
+        className="todo-input"
+      />
+      <button onClick={submitTodoHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
