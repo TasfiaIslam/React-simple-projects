3b06e458247de9b496c8fb3e8a91cd7345cc7554
Tasfia Islam <tasf1a027@gmail.com>
2020-12-12 12:26:09 +0600

    Fetch API

--- react-recipes/src/App.js
+++ react-recipes/src/App.js
@@ -1,7 +1,32 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const APP_ID = "4fa02d5a";
+  const APP_KEY = "bc90ade1e6be7d14dab595206551b450";
+
+  useEffect(() => {
+    console.log("something");
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = response.json();
+    console.log(data);
+  };
+
+  return (
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-btn" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default App;
